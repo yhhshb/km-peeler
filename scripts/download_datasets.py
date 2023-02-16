d1470508d00746391d35427ab133e671fd8d493f
Yoshihiro Shibuya <yoshihiro.shibuya@esiee.fr>
2023-02-16 14:20:03 +0100

    debug download script

--- scripts/download_datasets.py
+++ scripts/download_datasets.py
@@ -13,25 +13,31 @@ if __name__ == "__main__":
     print("Creating new dataset folders in {}".format(dataset_folder))
     if not dataset_folder.exists():
         os.makedirs(dataset_folder)
-    # salmonella_dataset_folder = dataset_folder.joinpath("salmonella")
-    # if not salmonella_dataset_folder.exists():
-    #     os.makedirs(salmonella_dataset_folder)
-    out = subprocess.run(["wget", salmonella_url], stderr=sys.stderr, stdout=subprocess.DEVNULL)
+    salmonella_dataset_folder = dataset_folder.joinpath("salmonella")
+    if not salmonella_dataset_folder.exists():
+        os.makedirs(salmonella_dataset_folder)
+    tuberculosis_dataset_folder = dataset_folder.joinpath("tuberculosis")
+    if not tuberculosis_dataset_folder.exists():
+        os.makedirs(tuberculosis_dataset_folder)
+
+    print("Downloading salmonella dataset")
+    out = subprocess.run(["wget", salmonella_url], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     if out.returncode != 0:
         raise RuntimeError("Unable to retrieve the salmonella dataset")
     salmonella_zip_file = pathlib.Path("assemblies.zip")
     assert salmonella_zip_file.exists()
-    salmonella_zip_file.rename(dataset_folder.joinpath(salmonella_zip_file))
+    salmonella_zip_file.rename(salmonella_dataset_folder.joinpath(salmonella_zip_file))
     
-    tuberculosis_dataset_folder = dataset_folder.joinpath("tuberculosis")
-    if not tuberculosis_dataset_folder.exists():
-        os.makedirs(tuberculosis_dataset_folder)
-    tuberculosis_accessions = pathlib.Path("tuberculosis_accessions.txt")
+    print("Downloading tuberculosis dataset")
+    this_path = pathlib.Path(sys.argv[0])
+    tuberculosis_accessions = this_path.joinpath("tuberculosis_accessions.txt")
     assert tuberculosis_accessions.exists()
     with open(tuberculosis_accessions, "r") as th:
         for accession in th:
             accession = accession.strip()
-            subprocess.run(["ncbi-acc-download", "--format", "fasta", accession], stderr=sys.stderr, stdout=subprocess.DEVNULL)
+            out = subprocess.run(["ncbi-acc-download", "--format", "fasta", accession], stderr=sys.stderr, stdout=subprocess.DEVNULL)
+            if not out.returncode == 0:
+                sys.stderr.write("Warning: {} probably failed to download".format(accession))
             downloaded = pathlib.Path(accession + ".fa") # Do not use with_suffix since names can be X.1
             assert downloaded.exists()
             downloaded.rename(tuberculosis_dataset_folder.joinpath(downloaded))
